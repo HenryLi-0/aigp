0f45b8c2219dcc533cfd4b6a192ed94983a39e31
HenryLi-0 <bxsci8266@gmail.com>
2026-09-24 11:49:59 -0400

    build actually works woo

--- lab 2/README.md
+++ lab 2/README.md
@@ -1,2 +1,7 @@
 # Lab-2---Cupcake-Collector
 cs class stuff, ignore
+
+
+use `python -m pygbag --port 1155 .` on school network (school network is silly)
+
+should exist on port 1155 as aforementioned
--- lab 2/main.py
+++ lab 2/main.py
@@ -12,59 +12,62 @@ async def main():
     pygame.display.set_caption("ooo")
     clock = pygame.time.Clock()
 
-    pygame.mixer.music.load("assets/Spring In My Step.ogg")
-    pygame.mixer.music.set_volume(0.25)
-    pygame.mixer.music.play(-1)
+    try:
+        pygame.mixer.music.load("assets/Spring_In_My_Step.ogg")
+        pygame.mixer.music.set_volume(0.25)
+        pygame.mixer.music.play(-1)
+    except:
+        print("sonion ring (no audio)")
 
-    class Constants:
-        PLAYER_HITBOX = (50, 50)
-        PLAYER_IMG = pygame.transform.scale(pygame.image.load("assets/player.png").convert_alpha(), PLAYER_HITBOX)
+    PLAYER_HITBOX = (50, 50)
+    PLAYER_IMG = pygame.transform.scale(pygame.image.load("assets/player.png").convert_alpha(), PLAYER_HITBOX)
 
-        COLLECTIBLE_HITBOX = (30, 30)
-        COLLECTIBLE_IMG = pygame.transform.scale(pygame.image.load("assets/normal.png").convert_alpha(), COLLECTIBLE_HITBOX)
+    COLLECTIBLE_HITBOX = (30, 30)
+    COLLECTIBLE_IMG = pygame.transform.scale(pygame.image.load("assets/normal.png").convert_alpha(), COLLECTIBLE_HITBOX)
 
-        BG = pygame.transform.scale(pygame.image.load("assets/nine paws.png").convert_alpha(), DISPLAY)
-        BG.set_alpha(150)
+    BG = pygame.transform.scale(pygame.image.load("assets/nine_paws.png").convert_alpha(), DISPLAY)
+    BG.set_alpha(150)
 
-        GRAVITY = 0.95
+    GRAVITY = 0.95
 
-        FITH = [pygame.mixer.Sound("assets/fith {}.ogg".format(x)) for x in range(1, 3+1)]
-        for sound in FITH: sound.set_volume(0.2)
+    FITH = [pygame.mixer.Sound("assets/fith_{}.ogg".format(x)) for x in range(1, 3+1)]
+    for sound in FITH: sound.set_volume(0.2)
 
-    class Map:
-        PLATFORMS = [
-            pygame.Rect(0, 350, 600, 50),
-            pygame.Rect(100, 230, 150, 10),
-            pygame.Rect(350, 170, 150, 10),
+    PLATFORMS = [
+        pygame.Rect(0, 350, 600, 50),
+        pygame.Rect(100, 230, 150, 10),
+        pygame.Rect(350, 170, 150, 10),
+    ]
+
+    COLLECTIBLES = [(125,200), (175,200), (225,200)] 
+    COLLECTIBLES_HITBOXES = []
+
+    def generateCollectibles(n):
+        nonlocal COLLECTIBLES_HITBOXES
+        for x in range(n): 
+            COLLECTIBLES.append((
+                random.randint(
+                    math.ceil(COLLECTIBLE_HITBOX[0]/2),
+                    math.floor(DISPLAY[0]-COLLECTIBLE_HITBOX[0]/2)),
+                random.randint(
+                    math.ceil(COLLECTIBLE_HITBOX[1]/2), 350)))
+        COLLECTIBLES_HITBOXES = [
+            pygame.Rect(
+                int(pos[0]-COLLECTIBLE_HITBOX[0]/2),
+                int(pos[1]-COLLECTIBLE_HITBOX[1]/2),
+                COLLECTIBLE_HITBOX[0],
+                COLLECTIBLE_HITBOX[1]
+            )
+            for pos in COLLECTIBLES
         ]
 
-        COLLECTIBLES = [(125,200), (175,200), (225,200)] 
-
-        def generateCollectibles(n):
-            for x in range(n): 
-                Map.COLLECTIBLES.append((
-                    random.randint(
-                        math.ceil(Constants.COLLECTIBLE_HITBOX[0]/2),
-                        math.floor(DISPLAY[0]-Constants.COLLECTIBLE_HITBOX[0]/2)),
-                    random.randint(
-                        math.ceil(Constants.COLLECTIBLE_HITBOX[1]/2), 350)))
-            Map.COLLECTIBLES_HITBOXES = [
-                pygame.Rect(
-                    int(pos[0]-Constants.COLLECTIBLE_HITBOX[0]/2),
-                    int(pos[1]-Constants.COLLECTIBLE_HITBOX[1]/2),
-                    Constants.COLLECTIBLE_HITBOX[0],
-                    Constants.COLLECTIBLE_HITBOX[1]
-                )
-                for pos in Map.COLLECTIBLES
-            ]
-
-    Map.generateCollectibles(0)
+    generateCollectibles(0)
 
     x = DISPLAY[0]//2
     y = DISPLAY[1]//2
     vx = 0
     vy = 0
-    r = Constants.PLAYER_HITBOX[0]//2
+    r = PLAYER_HITBOX[0]//2
 
     running = True
     ground = False
@@ -83,7 +86,7 @@ async def main():
         if keys[pygame.K_UP] and ground:
             vy = -20
 
-        vy += Constants.GRAVITY
+        vy += GRAVITY
         x += vx
         y += vy
 
@@ -104,77 +107,77 @@ async def main():
             vy=0
 
         playerRectGen = lambda: pygame.Rect(
-            int(x-Constants.PLAYER_HITBOX[0]/2),
-            int(y-Constants.PLAYER_HITBOX[1]/2),
-            Constants.PLAYER_HITBOX[0],
-            Constants.PLAYER_HITBOX[1]
+            int(x-PLAYER_HITBOX[0]/2),
+            int(y-PLAYER_HITBOX[1]/2),
+            PLAYER_HITBOX[0],
+            PLAYER_HITBOX[1]
         )
         playerRect = playerRectGen()
 
         # platform collisions of doom and despair
         ground = False
-        for platform in Map.PLATFORMS:
+        for platform in PLATFORMS:
             if playerRect.colliderect(platform):
                 if vy >= 0 and playerRect.bottom >= platform.top:
-                    y = platform.top - Constants.PLAYER_HITBOX[1] / 2
+                    y = platform.top - PLAYER_HITBOX[1] / 2
                     vy = 0
                     ground = True
                 elif vy < 0 and playerRect.top <= platform.bottom:
-                    y = platform.bottom + Constants.PLAYER_HITBOX[1] / 2
+                    y = platform.bottom + PLAYER_HITBOX[1] / 2
                     vy = 0
 
         playerRect = playerRectGen()
         
         # side collisions
-        for platform in Map.PLATFORMS:
+        for platform in PLATFORMS:
             if playerRect.colliderect(platform):
                 if vx > 0:
-                    x =platform.left-Constants.PLAYER_HITBOX[0]/2
+                    x =platform.left-PLAYER_HITBOX[0]/2
                     vx = 0
                 elif vx < 0:
-                    x = platform.right+Constants.PLAYER_HITBOX[0]/2
+                    x = platform.right+PLAYER_HITBOX[0]/2
                     vx = 0
 
 
         collected = []
-        for i, hitbox in enumerate(Map.COLLECTIBLES_HITBOXES):
+        for i, hitbox in enumerate(COLLECTIBLES_HITBOXES):
             if playerRect.colliderect(hitbox):
                 collected.append(i)
-                Constants.FITH[random.randint(0, len(Constants.FITH)-1)].play()
+                FITH[random.randint(0, len(FITH)-1)].play()
 
         for i in reversed(collected):
-            Map.COLLECTIBLES.pop(i)
-            Map.COLLECTIBLES_HITBOXES.pop(i)
+            COLLECTIBLES.pop(i)
+            COLLECTIBLES_HITBOXES.pop(i)
 
-        if len(Map.COLLECTIBLES)==0:
-            Map.generateCollectibles(random.randint(10,25)*random.randint(10,25))
+        if len(COLLECTIBLES)==0:
+            generateCollectibles(random.randint(10,25)*random.randint(10,25))
 
 
         color = math.sin(time.time()*10)*50+205
         screen.fill((color, color, color))
-        screen.blit(Constants.BG, (0, 0))
+        screen.blit(BG, (0, 0))
 
         ct = time.time()*2
         cr = int((math.sin(ct) + 1) * 127.5) + 50
         cg = int((math.sin(ct + 2) + 1) * 127.5) + 50
         cb = int((math.sin(ct + 4) + 1) * 127.5) + 50
         safe = lambda x: min(max(0, x), 255)
-        for platform in Map.PLATFORMS:
+        for platform in PLATFORMS:
             pygame.draw.rect(screen, (safe(cr), safe(cg), safe(cb)), platform)
         screen.blit(
-            Constants.PLAYER_IMG,
+            PLAYER_IMG,
             (
-                int(x-Constants.PLAYER_HITBOX[0]/2),
-                int(y-Constants.PLAYER_HITBOX[1]/2)
+                int(x-PLAYER_HITBOX[0]/2),
+                int(y-PLAYER_HITBOX[1]/2)
             )
         )
 
-        for pos in Map.COLLECTIBLES:
+        for pos in COLLECTIBLES:
             screen.blit(
-                Constants.COLLECTIBLE_IMG,
+                COLLECTIBLE_IMG,
                 (
-                    int(pos[0]-Constants.COLLECTIBLE_HITBOX[0]/2),
-                    int(pos[1]-Constants.COLLECTIBLE_HITBOX[1]/2)
+                    int(pos[0]-COLLECTIBLE_HITBOX[0]/2),
+                    int(pos[1]-COLLECTIBLE_HITBOX[1]/2)
                 )
             )
 
